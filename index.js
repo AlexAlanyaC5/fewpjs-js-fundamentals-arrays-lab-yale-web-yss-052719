c373a3068dfcd94778edfb7255dcf9c5a456188f
Alex Alanya <alexandro.alanya@gmail.com>
2019-07-08 21:14:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 const pets
 
 const pets = ["Milo", "Otis", "Garfield"]
-  //petsAppend.push("Odie")
+  pets.push("Odie")
 const pets = ["Milo", "Otis", "Garfield"]
-  //petsAppend.unshift("Odie")
+  pets.unshift("Odie")
 const pets = ["Milo", "Otis", "Garfield"]
-  //petsAppend.pop("Garfield")
+  pets.pop("Garfield")
 const pets = ["Milo", "Otis", "Garfield"]
-  //petsAppend.shift("Milo")
+  pets.shift("Milo")
