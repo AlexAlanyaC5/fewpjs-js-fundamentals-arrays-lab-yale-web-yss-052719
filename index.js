6637365b43e87b2eebc4229c7c3848fe2a82b8ed
Alex Alanya <alexandro.alanya@gmail.com>
2019-07-08 20:50:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 // Write your solution here!
-const petsAppend = ["Milo", "Otis", "Garfield", "Odie"]
+const petsAppend = ["Milo", "Otis", "Garfield"]
+const petsPrepend = ["Milo", "Otis", "Garfield"]
+const petsRemoveLast = ["Milo", "Otis", "Garfield"]
+const petsRemoveFirst = ["Milo", "Otis", "Garfield"]
