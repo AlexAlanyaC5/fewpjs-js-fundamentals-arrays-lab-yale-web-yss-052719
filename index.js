b526c1faf10251a721f7615816767ebad59f3732
Alex Alanya <alexandro.alanya@gmail.com>
2019-07-08 21:07:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // Write your solution here!
+const pets = "petsAppend"
+
 const petsAppend = ["Milo", "Otis", "Garfield"]
-  petsAppend.push("Odie")
+  //petsAppend.push("Odie")
 const petsPrepend = ["Milo", "Otis", "Garfield"]
-  petsAppend.unshift("Odie")
+  //petsAppend.unshift("Odie")
 const petsRemoveLast = ["Milo", "Otis", "Garfield"]
-  petsAppend.pop("Garfield")
+  //petsAppend.pop("Garfield")
 const petsRemoveFirst = ["Milo", "Otis", "Garfield"]
-  petsAppend.shift("Milo")
+  //petsAppend.shift("Milo")
